d62390638f7b142ef8797023a5c5a486118e1f41
Safayet <hossainsafayet187@gmail.com>
2024-04-03 01:35:22 +0600

    search option added

--- src/pages/AllReceiveData.js
+++ src/pages/AllReceiveData.js
@@ -1,40 +1,152 @@
-import { Table } from "antd";
-
+import { SearchOutlined } from '@ant-design/icons';
+import { Button, Input, Space, Table } from 'antd';
+import Highlighter from 'react-highlight-words';
 import { useDispatch, useSelector } from "react-redux";
 
 import { getAllSellDetails } from "../features/sell/sellSlice";
-import { useEffect, useState } from "react";
-
+import React, { useEffect, useRef, useState } from 'react';
 import { getCustomers } from "../features/customer/customerSlice";
 import { getAllReceiveData } from "../features/receive/receiveSlice";
 import { ClipLoader } from "react-spinners";
 import { TbCurrencyTaka } from "react-icons/tb";
 
-const rcv_column = [
-  {
-    title: "SNo",
-    dataIndex: "key",
-    sorter: (a, b) => a.key - b.key,
-  },
-  {
-    title: "Date",
-    dataIndex: "date",
-  },
-  {
-    title: "Customer Name",
-    dataIndex: "name",
-  },
-  {
-    title: "Description",
-    dataIndex: "description",
-  },
 
-  {
-    title: "Amount",
-    dataIndex: "amount",
-  },
-];
+
+const onChange = (pagination, filters, sorter, extra) => {
+  console.log('params', pagination, filters, sorter, extra);
+};
 const AllReceiveData = () => {
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef(null);
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: 'block',
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? '#1677ff' : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: '#ffc069',
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+  const rcv_column = [
+    {
+      title: "SNo",
+      dataIndex: "key",
+      sorter: (a, b) => a.key - b.key,
+    },
+    {
+      title: "Date",
+      dataIndex: "date",
+    },
+    {
+      title: "Customer Name",
+      dataIndex: "name",
+      ...getColumnSearchProps('name'),
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+    },
+  
+    {
+      title: "Amount",
+      dataIndex: "amount",
+    },
+  ];
   const [totalRcv, setTotalRcv] = useState(null);
   const dispatch = useDispatch();
   const changeDateFormat = (date) => {
@@ -63,6 +175,7 @@ const AllReceiveData = () => {
           description: receive_state[index].description,
 
           amount: receive_state[index].amount,
+
         });
       }
     }
@@ -87,7 +200,7 @@ const AllReceiveData = () => {
           {" "}
           <h3 className="mb-4 title mt-4">Total Receive : {totalRcv} <TbCurrencyTaka/> </h3>
           <div>
-            <Table columns={rcv_column} dataSource={receiveData} scroll={{
+            <Table columns={rcv_column}  dataSource={receiveData} scroll={{
                     x: 700,
                   }} />
           </div>
